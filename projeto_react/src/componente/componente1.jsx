dd4bf3c3d5f021523911cc52b8f9a0e6eed63cfb
jbonacini <jbonacini@gmail.com>
2022-02-19 15:50:49 +0000

    Aula (19/02/2022)

---
+++ projeto_react/src/componente/componente1.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+// function func1() {
+//     return <h1> PRIMEIRO COMPONENTE </h1>
+// }
+
+// export default () => <h1> Nosso Primeiro Componente Arrow</h1>
+
+export default () => (15+22)
---
+++ projeto_react/src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import componente1 from './componente/componente1'
+
+const element = document.getElementById('root')
+
+ReactDOM.render(
+    <div>
+        <componente1 />
+    </div>
+        , element
+)
