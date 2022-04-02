dab97bdd6789255fbe52b8b4127fb1c6f3d1128a
jbonacini <jbonacini@gmail.com>
2022-04-02 14:01:20 +0000

    02/04/2022 - Final

--- projeto_react/src/components/Intervalo.jsx
+++ projeto_react/src/components/Intervalo.jsx
@@ -3,25 +3,41 @@ import React from "react"
 import Card from "./Card"
 
 import { connect } from "react-redux"
+import { alteraNumeroMinimo, alteraNumeroMaximo } from "../store/Action/numeros"
 
 function Intervalo(props) {
-    const {min, max} = props
-    return(
-        <Card title = "Intervalo de Números" red>
+    const { min, max } = props
+    return (
+        <Card title="Intervalo de Números" red>
             <div className="Intervalo">
                 <span>
                     <span>Mínimo:</span>
                     {/* <input type="number" value={min} onChange={e => props.onMinChanged(+e.target.value)} /> */}
-                    <input type="number" value={min}/>
+                    <input type="number" value={min}
+                        onChange={e => props.alterarMinino(+e.target.value)} />
                 </span>
                 <span>
                     <span>Máximo:</span>
                     {/* <input type="number" value={max} onChange={e => props.onMaxChanged(+e.target.value)} /> */}
-                    <input type="number" value={max}/>
+                    <input type="number" value={max}
+                        onChange={e => props.alterarMaximo(+e.target.value)} />
                 </span>
             </div>
         </Card>
     )
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        alterarMinino(novoNumero) {
+            const action = alteraNumeroMinimo(novoNumero)
+            dispatch(action)
+        },
+        alterarMaximo(novoNumero) {
+            const action = alteraNumeroMaximo(novoNumero)
+            dispatch(action)
+        }
+    }
 }
 
 function mapStateToProps(state) {
@@ -31,4 +47,4 @@ function mapStateToProps(state) {
     }
 }
 
-export default connect(mapStateToProps)(Intervalo)
+export default connect(mapStateToProps, mapDispatchToProps)(Intervalo)
--- projeto_react/src/store/storeConfig/storeConfig.js
+++ projeto_react/src/store/storeConfig/storeConfig.js
@@ -1,15 +1,29 @@
-import {createStore, combineReducers} from 'redux';
+import { createStore, combineReducers } from 'redux';
 
 const reducers = combineReducers({
-    numeros: function(state, action){
-        return{
-            min: 5,
-            max: 35
+    numeros: function (state, action) {
+
+        switch (action.type) {
+            case 'NUM_MIN_ALTERADO':
+                return {
+                    ...state,
+                    min: action.payload
+                }
+            case 'NUM_MAX_ALTERADO':
+                return {
+                    ...state,
+                    max: action.payload
+                }
+            default:
+                return {
+                    min: 5,
+                    max: 35
+                }
         }
     }
 })
 
-function storeConfig(){
+function storeConfig() {
     return createStore(reducers)
 }
 
